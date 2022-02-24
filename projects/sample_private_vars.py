0b12b1590798956031fd558ddceed433fd6a3698
johnedstone <johnedstone@gmail.com>
2022-02-24 03:10:04 -0500

    cleaning up

--- projects/sample_private_vars.py
+++ projects/sample_private_vars.py
@@ -1,2 +1,4 @@
+# Place this in the lib folder with your private variables
+# Rename to private_vars.py
 HOST = "your.restapi.domain" ## e.g "myserver.net"
 PATH = "/your/api/endpoint/"  ## e.g. "/api/v1/endpoint/"
