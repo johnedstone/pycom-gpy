e93d5940e8c76190693f08eb236f1d2d50fb0f77
johnedstone <johnedstone@gmail.com>
2022-02-26 00:14:27 -0500

    working on tracking

---
+++ simple_files/pytrack/pytrack_example.py
@@ -0,0 +1,38 @@
+'''
+https://docs.pycom.io/tutorials/expansionboards/tracking/
+'''
+#from network import WLAN
+#import time
+#import machine
+#import private_vars
+#
+#import math
+#import os
+#import gc
+#import pycom
+#from L76GNSS import L76GNSS
+from pytrack_2 import Pytrack_cannt_find_this_class_but_works
+
+#wlan = WLAN(mode=WLAN.STA)
+#wlan.connect(ssid='{}'.format(private_vars.SSID), auth=(WLAN.WPA2, '{}'.format(private_vars.SSID_PASSWORD))) #for the connection details, check your router.
+ 
+#pycom.heartbeat(False)
+#pycom.rgbled(0x0A0A08) # white
+ 
+#time.sleep(2)
+#gc.enable()
+# 
+#py = Pytrack()
+ 
+#time.sleep(1)
+#l76 = L76GNSS(py, timeout=30, buffer=512)
+# 
+#time.sleep(1)
+# 
+#while (True):
+#    coord = l76.coordinates()
+#    print("{} - {}".format(coord, gc.mem_free()))
+#    print('Sleep for 1 minute.')
+#    time.sleep(60)
+
+# vim: ai et ts=4 sts=4 sw=4 nu
